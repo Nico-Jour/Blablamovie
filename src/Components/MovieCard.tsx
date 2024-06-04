d87225c35556f49648388a28399aa5978c4b0a15
Monoceros-frontend <monoceros.frontend@gmail.com>
2024-06-04 21:37:20 +0200

    movieCard styling

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -5,16 +5,19 @@ import { MoviePreview } from "../types";
 export default function MovieCard({ movie }: { movie: MoviePreview }) {
   const navigate = useNavigate();
   return (
-    <Box>
+    <Box mb={3}>
       <Box
         onClick={() => navigate(`/movie/${movie.imdbID}`)}
         sx={{ cursor: "pointer" }}
       >
         <img src={movie.Poster} />
       </Box>
-      <Box>
+      <Box display={"flex"}>
+        <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+          {movie.Year}
+        </Typography>
         <Typography variant="h6" gutterBottom>
-          {movie.Year} {movie.Title}
+          , {movie.Title}
         </Typography>
       </Box>
     </Box>
