b6e2e401bbbb96d3dc5617b3d083a8f68f8c2452
Monoceros-frontend <monoceros.frontend@gmail.com>
2024-06-05 10:43:46 +0200

    refetch when logging

--- src/Hooks/useFavoriteMovies.tsx
+++ src/Hooks/useFavoriteMovies.tsx
@@ -24,7 +24,7 @@ export default function useFavoriteMovies() {
 
   useEffect(() => {
     fetchFavoriteMovies();
-  }, []);
+  }, [user]);
 
   return { favoriteMovies, loading, error, refetch: fetchFavoriteMovies };
 }
